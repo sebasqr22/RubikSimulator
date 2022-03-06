42ca9bc84d523f1bb0d36fe5276d1fc5bf30139a
sebasqr22 <63075293+sebasqr22@users.noreply.github.com>
2022-03-06 12:55:30 -0600

    Se agrega la c2a y se cambia la logica de columnas y filas a archivos por separado

---
+++ c1.rkt
@@ -0,0 +1,97 @@
+#lang racket
+
+(define (position matriz i j)
+  (list-ref (list-ref matriz i) j))
+
+
+(define (c1b matriz nueva contador type);; se mueve 0, 3, 6
+  (cond [(equal? type 3)
+         (cond [(equal? contador 0)
+                (c1b matriz (cons (list (position matriz 3 0) (position matriz 0 1) (position matriz 0 2) (position matriz 3 3) (position matriz 0 4) (position matriz 0 5) (position matriz 3 6) (position matriz 0 7) (position matriz 0 8)) nueva) (+ 1 contador) type)] ;; blanca
+               [(equal? contador 1)
+                (c1b matriz (cons (list (position matriz 0 0) (position matriz 1 1) (position matriz 1 2) (position matriz 0 3) (position matriz 1 4) (position matriz 1 5) (position matriz 0 6) (position matriz 1 7) (position matriz 1 8)) nueva) (+ 1 contador) type)];; naranja
+               [(equal? contador 2)
+                (c1b matriz (cons (list (position matriz 1 0) (position matriz 2 1) (position matriz 2 2) (position matriz 1 3) (position matriz 2 4) (position matriz 2 5) (position matriz 1 6) (position matriz 2 7) (position matriz 2 8)) nueva) (+ 1 contador) type)];; amarillo
+               [(equal? contador 3)
+                (c1b matriz (cons (list (position matriz 2 0) (position matriz 3 1) (position matriz 3 2) (position matriz 2 3) (position matriz 3 4) (position matriz 3 5) (position matriz 2 6) (position matriz 3 7) (position matriz 3 8)) nueva) (+ 1 contador) type)];; rojo
+               [(equal? contador 4)
+                (c1b matriz (cons (list-ref matriz 4) nueva) (+ 1 contador) type)];; verde
+               [(equal? contador 5)
+                (c1b matriz (cons (list (position matriz 5 6) (position matriz 5 3) (position matriz 5 0) (position matriz 5 7) (position matriz 5 4) (position matriz 5 1) (position matriz 5 8) (position matriz 5 5) (position matriz 5 2)) nueva) (+ 1 contador) type)];;azul
+               [else (reverse nueva)])]
+        
+        [(equal? type 2)
+         (cond [(equal? contador 0)
+                (c1b matriz (cons (list (position matriz 3 0) (position matriz 0 1) (position matriz 3 2) (position matriz 0 3)) nueva) (+ 1 contador) type)] ;;blanco
+               [(equal? contador 1)
+                (c1b matriz (cons (list (position matriz 0 0) (position matriz 1 1) (position matriz 0 2) (position matriz 1 3)) nueva) (+ 1 contador) type)] ;;naranja
+               [(equal? contador 2)
+                (c1b matriz (cons (list (position matriz 1 0) (position matriz 2 1) (position matriz 1 2) (position matriz 2 3)) nueva) (+ 1 contador) type)] ;amarillo
+               [(equal? contador 3)
+                (c1b matriz (cons (list (position matriz 2 0) (position matriz 3 1) (position matriz 2 2) (position matriz 3 3)) nueva) (+ 1 contador) type)] ;;roja
+               [(equal? contador 4)
+                (c1b matriz (cons (list-ref matriz 4) nueva) (+ 1 contador) type)] ;;verde
+               [(equal? contador 5)
+                (c1b matriz (cons (list (position matriz 5 2) (position matriz 5 0) (position matriz 5 3) (position matriz 5 1)) nueva) (+ 1 contador) type)] ;;azul
+               [else (reverse nueva)])]
+
+        [(equal? type 4)
+         (cond [(equal? contador 0)
+                (c1b matriz (cons (list (position matriz 3 0) (position matriz 0 1) (position matriz 0 2) (position matriz 0 3) (position matriz 3 4) (position matriz 0 5) (position matriz 0 6) (position matriz 0 7) (position matriz 3 8) (position matriz 0 9) (position matriz 0 10) (position matriz 0 11) (position matriz 3 12) (position matriz 0 13) (position matriz 0 14) (position matriz 0 15)) nueva) (+ 1 contador) type)] ;;blanco
+               [(equal? contador 1)
+                (c1b matriz (cons (list (position matriz 0 0) (position matriz 1 1) (position matriz 1 2) (position matriz 1 3) (position matriz 0 4) (position matriz 1 5) (position matriz 1 6) (position matriz 1 7) (position matriz 0 8) (position matriz 1 9) (position matriz 1 10) (position matriz 1 11) (position matriz 0 12) (position matriz 1 13) (position matriz 1 14) (position matriz 1 15)) nueva) (+ 1 contador) type)] ;;naranja
+               [(equal? contador 2)
+                (c1b matriz (cons (list (position matriz 1 0) (position matriz 2 1) (position matriz 2 2) (position matriz 2 3) (position matriz 1 4) (position matriz 2 5) (position matriz 2 6) (position matriz 2 7) (position matriz 1 8) (position matriz 2 9) (position matriz 2 10) (position matriz 2 11) (position matriz 1 12) (position matriz 2 13) (position matriz 2 14) (position matriz 2 15)) nueva) (+ 1 contador) type)] ;;amarillo
+               [(equal? contador 3)
+                (c1b matriz (cons (list (position matriz 2 0) (position matriz 3 1) (position matriz 3 2) (position matriz 3 3) (position matriz 2 4) (position matriz 3 5) (position matriz 3 6) (position matriz 3 7) (position matriz 2 8) (position matriz 3 9) (position matriz 3 10) (position matriz 3 11) (position matriz 2 12) (position matriz 3 13) (position matriz 3 14) (position matriz 3 15)) nueva) (+ 1 contador) type )] ;;rojo
+               [(equal? contador 4)
+                (c1b matriz (cons (list-ref matriz 4) nueva) (+ 1 contador) type)] ;;verde
+               [(equal? contador 5)
+                (c1b matriz (cons (list (position matriz 5 12) (position matriz 5 8) (position matriz 5 4) (position matriz 5 0) (position matriz 5 13) (position matriz 5 9) (position matriz 5 5) (position matriz 5 1) (position matriz 5 14) (position matriz 5 10) (position matriz 5 6) (position matriz 5 2) (position matriz 5 15) (position matriz 5 11) (position matriz 5 7) (position matriz 5 3)) nueva) (+ 1 contador) type )] ;;azul
+               [else (reverse nueva)])]))
+
+(define (c1a matriz nueva contador type);; se mueve 0, 3, 6
+  (cond [(equal? type 3)
+         (cond [(equal? contador 0)
+                (c1a matriz (cons (list (position matriz 1 0) (position matriz 0 1) (position matriz 0 2) (position matriz 1 3) (position matriz 0 4) (position matriz 0 5) (position matriz 1 6) (position matriz 0 7) (position matriz 0 8)) nueva) (+ 1 contador) type)] ;; blanca
+               [(equal? contador 1)
+                (c1a matriz (cons (list (position matriz 2 0) (position matriz 1 1) (position matriz 1 2) (position matriz 2 3) (position matriz 1 4) (position matriz 1 5) (position matriz 2 6) (position matriz 1 7) (position matriz 1 8)) nueva) (+ 1 contador) type)];; naranja
+               [(equal? contador 2)
+                (c1a matriz (cons (list (position matriz 3 0) (position matriz 2 1) (position matriz 2 2) (position matriz 3 3) (position matriz 2 4) (position matriz 2 5) (position matriz 3 6) (position matriz 2 7) (position matriz 2 8)) nueva) (+ 1 contador) type)];; amarillo
+               [(equal? contador 3)
+                (c1a matriz (cons (list (position matriz 0 0) (position matriz 3 1) (position matriz 3 2) (position matriz 0 3) (position matriz 3 4) (position matriz 3 5) (position matriz 0 6) (position matriz 3 7) (position matriz 3 8)) nueva) (+ 1 contador) type)];; rojo
+               [(equal? contador 4)
+                (c1a matriz (cons (list-ref matriz 4) nueva) (+ 1 contador) type)];; verde
+               [(equal? contador 5)
+                (c1a matriz (cons (list (position matriz 5 2) (position matriz 5 5) (position matriz 5 8) (position matriz 5 1) (position matriz 5 4) (position matriz 5 7) (position matriz 5 0) (position matriz 5 3) (position matriz 5 6)) nueva) (+ 1 contador) type)];;azul
+               [else (reverse nueva)])]
+        
+        [(equal? type 2)
+         (cond [(equal? contador 0)
+                (c1a matriz (cons (list (position matriz 1 0) (position matriz 0 1) (position matriz 1 2) (position matriz 0 3)) nueva) (+ 1 contador) type)] ;;blanco
+               [(equal? contador 1)
+                (c1a matriz (cons (list (position matriz 2 0) (position matriz 1 1) (position matriz 2 2) (position matriz 1 3)) nueva) (+ 1 contador) type)] ;;naranja
+               [(equal? contador 2)
+                (c1a matriz (cons (list (position matriz 3 0) (position matriz 2 1) (position matriz 3 2) (position matriz 2 3)) nueva) (+ 1 contador) type)] ;amarillo
+               [(equal? contador 3)
+                (c1a matriz (cons (list (position matriz 0 0) (position matriz 3 1) (position matriz 0 2) (position matriz 3 3)) nueva) (+ 1 contador) type)] ;;roja
+               [(equal? contador 4)
+                (c1a matriz (cons (list-ref matriz 4) nueva) (+ 1 contador) type)] ;;verde
+               [(equal? contador 5)
+                (c1a matriz (cons (list (position matriz 5 1) (position matriz 5 3) (position matriz 5 0) (position matriz 5 2)) nueva) (+ 1 contador) type)] ;;azul
+               [else (reverse nueva)])]
+
+        [(equal? type 4)
+         (cond [(equal? contador 0)
+                (c1a matriz (cons (list (position matriz 1 0) (position matriz 0 1) (position matriz 0 2) (position matriz 0 3) (position matriz 1 4) (position matriz 0 5) (position matriz 0 6) (position matriz 0 7) (position matriz 1 8) (position matriz 0 9) (position matriz 0 10) (position matriz 0 11) (position matriz 1 12) (position matriz 0 13) (position matriz 0 14) (position matriz 0 15)) nueva) (+ 1 contador) type)] ;;blanco
+               [(equal? contador 1)
+                (c1a matriz (cons (list (position matriz 2 0) (position matriz 1 1) (position matriz 1 2) (position matriz 1 3) (position matriz 2 4) (position matriz 1 5) (position matriz 1 6) (position matriz 1 7) (position matriz 2 8) (position matriz 1 9) (position matriz 1 10) (position matriz 1 11) (position matriz 2 12) (position matriz 1 13) (position matriz 1 14) (position matriz 1 15)) nueva) (+ 1 contador) type)] ;;naranja
+               [(equal? contador 2)
+                (c1a matriz (cons (list (position matriz 3 0) (position matriz 2 1) (position matriz 2 2) (position matriz 2 3) (position matriz 3 4) (position matriz 2 5) (position matriz 2 6) (position matriz 2 7) (position matriz 3 8) (position matriz 2 9) (position matriz 2 10) (position matriz 2 11) (position matriz 3 12) (position matriz 2 13) (position matriz 2 14) (position matriz 2 15)) nueva) (+ 1 contador) type)] ;;amarillo
+               [(equal? contador 3)
+                (c1a matriz (cons (list (position matriz 0 0) (position matriz 3 1) (position matriz 3 2) (position matriz 3 3) (position matriz 0 4) (position matriz 3 5) (position matriz 3 6) (position matriz 3 7) (position matriz 0 8) (position matriz 3 9) (position matriz 3 10) (position matriz 3 11) (position matriz 0 12) (position matriz 3 13) (position matriz 3 14) (position matriz 3 15)) nueva) (+ 1 contador) type )] ;;rojo
+               [(equal? contador 4)
+                (c1a matriz (cons (list-ref matriz 4) nueva) (+ 1 contador) type)] ;;verde
+               [(equal? contador 5)
+                (c1a matriz (cons (list (position matriz 5 3) (position matriz 5 7) (position matriz 5 11) (position matriz 5 15) (position matriz 5 2) (position matriz 5 6) (position matriz 5 10) (position matriz 5 14) (position matriz 5 1) (position matriz 5 5) (position matriz 5 9) (position matriz 5 13) (position matriz 5 0) (position matriz 5 4) (position matriz 5 8) (position matriz 5 12)) nueva) (+ 1 contador) type )] ;;azul
+               [else (reverse nueva)])]))
